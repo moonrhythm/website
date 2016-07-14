e4f923524caf3480617911844524f238c03fd32b
Thanatat Tamtan <acoshift@gmail.com>
2016-07-14 09:23:11 +0700

    init

---
+++ entry.js
@@ -0,0 +1,17 @@
+require('./bower_components/bootstrap/dist/css/bootstrap.min.css');
+require('./bower_components/font-awesome/css/font-awesome.min.css');
+require('./bower_components/lato/css/lato.min.css');
+require('./bower_components/octicons/octicons/octicons.css');
+require('./css/fonts.css');
+require('./node_modules/typeit/dist/typeit.min.css');
+require('./css/app.css');
+require('script!./bower_components/jquery/dist/jquery.min.js');
+require('script!./bower_components/jquery-easing/jquery.easing.min.js');
+require('script!./bower_components/bootstrap/dist/js/bootstrap.min.js');
+require('script!./bower_components/jqBootstrapValidation/src/jqBootstrapValidation.js');
+require('script!./bower_components/classie/classie.js');
+require('script!./node_modules/typeit/dist/typeit.min.js');
+//require('script!./js/cbpAnimatedHeader.min.js');
+require('script!./js/contact_me.js');
+require('script!./js/app.js');
+require('script!./js/google-analytics.js');
---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+var webpack = require("webpack");
+
+module.exports = {
+  target: 'web',
+  context: __dirname,
+  entry: "./entry.js",
+  output: {
+    path: __dirname + '/dist',
+    filename: "bundle.js"
+  },
+  //devtool: "source-map",
+  module: {
+    loaders: [
+      { test: /\.css$/, loader: 'style!css' },
+      { test: /\.(ttf|eot|svg|woff(2)?)(\?[a-z0-9\.\=]+)?$/, loader: 'file?name=./fonts/[hash].[ext]' },
+      { test: /\.(jpe?g|png|gif)$/i, loader: 'file?name=./img/[hash].[ext]' },
+    ]
+  },
+  plugins: [
+    new webpack.optimize.DedupePlugin(),
+    new webpack.optimize.UglifyJsPlugin({ minimize: true, comments: false }),
+  ],
+  resolve: {
+    modulesDirectories: ['node_modules', 'bower_components'],
+  }
+};
