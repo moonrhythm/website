c170592dbf32313e23ece7277f6332599417010a
acoshift <acoshift@gmail.com>
2016-08-07 16:43:15 +0700

    update

--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,25 @@
-var webpack = require("webpack");
+const webpack = require('webpack')
 
 module.exports = {
   target: 'web',
-  context: __dirname,
-  entry: "./entry.js",
+  entry: './entry.js',
   output: {
-    path: __dirname + '/build',
-    filename: "bundle.js"
+    path: './build',
+    filename: 'bundle.js'
   },
-  //devtool: "source-map",
+  devtool: null,
   module: {
     loaders: [
       { test: /\.css$/, loader: 'style!css' },
-      { test: /\.(ttf|eot|svg|woff(2)?)(\?[a-z0-9\.\=]+)?$/, loader: 'file?name=./fonts/[hash].[ext]' },
-      { test: /\.(jpe?g|png|gif)$/i, loader: 'file?name=./img/[hash].[ext]' },
+      { test: /\.(ttf|eot|svg|woff(2)?)(\?[a-z0-9\.=]+)?$/, loader: 'file?name=./fonts/[hash].[ext]' },
+      { test: /\.(jpe?g|png|gif)$/i, loader: 'file?name=./img/[hash].[ext]' }
     ]
   },
   plugins: [
     new webpack.optimize.DedupePlugin(),
-    new webpack.optimize.UglifyJsPlugin({ minimize: true, comments: false }),
+    new webpack.optimize.UglifyJsPlugin({ minimize: true, comments: false })
   ],
   resolve: {
-    modulesDirectories: ['node_modules', 'bower_components'],
+    modulesDirectories: ['node_modules', 'bower_components']
   }
-};
+}
