c371f394eea7467cd10b0bf7ca47a45555d2d098
anthoz69 <st.anthoz69@gmail.com>
2018-12-06 12:14:26 +0700

    setup style

---
+++ Gulpfile.js
@@ -0,0 +1,29 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const autoprefixer = require('gulp-autoprefixer');
+const rename = require('gulp-rename');
+
+const output = './assets/css'
+const sassOption = {
+	outputStyle: 'compressed',
+	includePaths: 'node_modules/'
+}
+
+gulp.task('dev', function () {
+	build('./src/sass/main.scss', 'moonrhythm')
+})
+
+function build(source, basename, compress = false, suffix = '.min') {
+	sassOption.outputStyle = compress == true ? 'compressed' : 'expanded'
+	gulp.src(source)
+	.pipe(sass({outputStyle: sassOption}).on('error', sass.logError))
+	.pipe(autoprefixer({
+		browsers: ['last 2 versions']
+	}))
+	.pipe(rename({
+		basename: basename,
+		suffix: suffix,
+		extname: '.css'
+	}))
+	.pipe(gulp.dest(output))
+}
