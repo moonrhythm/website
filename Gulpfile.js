5f19a62bcf91fe9a0965d7abbb22e6f99ad6ac8f
anthoz69 <st.anthoz69@gmail.com>
2018-12-07 19:14:17 +0700

    purge css for speed.

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,7 +1,8 @@
-const gulp = require('gulp');
-const sass = require('gulp-sass');
-const autoprefixer = require('gulp-autoprefixer');
-const rename = require('gulp-rename');
+const gulp = require('gulp')
+const sass = require('gulp-sass')
+const autoprefixer = require('gulp-autoprefixer')
+const rename = require('gulp-rename')
+const purgecss = require('gulp-purgecss')
 
 const output = './assets/css'
 const sassOption = {
@@ -13,12 +14,17 @@ gulp.task('dev', function () {
 	build('./src/sass/main.scss', 'moonrhythm')
 })
 
+gulp.task('prod', function () {
+	build('./src/sass/main.scss', 'moonrhythm', true)
+})
+
 gulp.task('watch', () => gulp.watch('src/sass/**/*.scss', ['dev']))
 
 function build(source, basename, compress = false, suffix = '.min') {
 	sassOption.outputStyle = compress == true ? 'compressed' : 'expanded'
 	gulp.src(source)
 	.pipe(sass(sassOption).on('error', sass.logError))
+	.pipe(purgecss({ content: ['./src/**/*.html'] }))
 	.pipe(autoprefixer({
 		browsers: ['last 2 versions']
 	}))
